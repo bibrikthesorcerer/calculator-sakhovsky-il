96dab3683c1878ad2ce35bf36fae55aaf691f70f
bibrikthesorcerer <ignatsahovsky@gmail.com>
2025-03-06 17:49:43 +0300

    SAT-1: Added module parameters - custom address and port

--- calc_server.py
+++ calc_server.py
@@ -1,3 +1,4 @@
+import argparse
 from http.server import HTTPServer
 from functools import cached_property
 from http.server import BaseHTTPRequestHandler
@@ -10,11 +11,38 @@ class CalculatorRequestHandler(BaseHTTPRequestHandler):
     pass
 
 
-if __name__ == "__main__":
-    server = HTTPServer(("0.0.0.0", 8000), CalculatorRequestHandler)
+def parse_args() -> argparse.Namespace:
+    """Returns parsed Namespace of module arguments"""
+    parser = argparse.ArgumentParser()
+    # positional args
+    parser.add_argument("address", nargs="?", help="Server Address", type=str, default="0.0.0.0")
+    parser.add_argument("port", nargs="?", help="Server Port", type=int, default=8000)
+    # flag-based args
+    parser.add_argument("-a", "--Address", dest="address", help="Server Address (overrides positional argument)", type=str)
+    parser.add_argument("-p", "--Port", dest="port", help="Server Port (overrides positional argument)", type=int)
+    return parser.parse_args()
+
+def run_calc_server(address: str, port: int):
+    # init server
     try:
-        print("Server is up!")
+        server = HTTPServer((address, port), CalculatorRequestHandler)
+    except Exception as e:
+        print(f"Server initialization Error! {str(e)}")
+        exit(1)
+    
+    # run server
+    try:
+        print(f"Server is up on {address}:{port}")
         server.serve_forever()
     except KeyboardInterrupt:
+        print("Received shutdown signal")
+    finally:
         server.server_close()
         print("Server is down!")
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    address = args.address
+    port = args.port
+    run_calc_server(address, port)
