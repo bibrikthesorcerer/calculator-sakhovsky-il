70a68bbe7f14c7da01618169e58e63f65b3c000b
bibrikthesorcerer <ignatsahovsky@gmail.com>
2025-03-06 20:02:33 +0300

    SAT-2: Added request validation. Content-Type, query and requestBody are being validated now

--- calc_server.py
+++ calc_server.py
@@ -1,6 +1,7 @@
 import json
 import argparse
 from typing import Any
+from http import HTTPStatus
 from http.server import HTTPServer
 from functools import cached_property
 from http.server import BaseHTTPRequestHandler
@@ -25,16 +26,58 @@ class CalculatorRequestHandler(BaseHTTPRequestHandler):
         content_length = int(self.headers.get("Content-Length", 0))
         return self.rfile.read(content_length)
     
-    def _send_json_response(self, resp_code, resp_body):
-        # add response code and headers to header buffer
-        self.send_response(resp_code)
-        self.send_header("Content-Type", "application/json")
-        self.end_headers()
-        # write response to output stream
-        self.wfile.write(resp_body)
+    def _make_error_body(self, msg: str, **kwargs) -> bytes:
+        response_body = json.dumps({
+                "error": msg,
+                **kwargs
+            })
+        return response_body.encode("utf-8")
+    
+    def _validate_request(self) -> bool | tuple[bool, str]:
+        # content-type validation
+        content_type = self.headers.get('Content-Type', '')
+        if not content_type.startswith('application/json'):
+            raise Exception(
+                HTTPStatus.UNSUPPORTED_MEDIA_TYPE.value,
+                self._make_error_body("Invalid Content-Type - must be application/json")
+            )
+        
+        # float-mode validation
+        float_mode = self.query_data.get("float", "false")
+        if float_mode not in ["false", "true"]:
+            raise Exception(
+                HTTPStatus.BAD_REQUEST.value,
+                self._make_error_body("Incorrect float value")
+            )
+        
+        # requestBody data validation
+        if not self.post_data:
+            raise Exception(
+                HTTPStatus.BAD_REQUEST.value,
+                self._make_error_body("Empty requestBody")
+            )
+        
+        input_data = json.loads(self.post_data)
+        if not isinstance(input_data, str):
+            raise Exception(
+                HTTPStatus.BAD_REQUEST.value,
+                self._make_error_body("Incorrect input data", input=input_data)
+            )
+        
+        return (float_mode, input_data)
 
-    def do_POST(self):
-        resp_dict = dict()
+
+    def _calc_post_response(self) -> tuple[int, bytes]:
+        # validate input
+        try:
+            float_mode, input_data = self._validate_request()
+        except Exception as e:
+            return e.args
+        
+        resp_dict = {
+            "validated_mode": float_mode,
+            "validated_input": input_data
+        }
         if self.url.path:
             resp_dict["path"] = self.url.path
         if self.query_data:
@@ -43,7 +86,20 @@ class CalculatorRequestHandler(BaseHTTPRequestHandler):
             resp_dict["requestBody"] = self.post_data.decode("utf-8")
         
         resp_body = json.dumps(resp_dict)
-        self._send_json_response(200, resp_body.encode("utf-8"))
+
+        return (HTTPStatus.OK.value, resp_body.encode("utf-8"))
+        
+    def _send_json_response(self, resp_code: int, resp_body: bytes):
+        # add response code and headers to header buffer
+        self.send_response(resp_code)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        # write response to output stream
+        self.wfile.write(resp_body)
+
+    def do_POST(self):
+        resp_code, resp_body = self._calc_post_response()
+        self._send_json_response(resp_code, resp_body)
 
 
 def parse_args() -> argparse.Namespace:
